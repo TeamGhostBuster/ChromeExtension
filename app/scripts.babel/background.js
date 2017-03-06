b08476e14730eaf39cc17fe1de9114bd9dfc32f0
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-06 10:39:08 -0700

    added more oath stuff, created react + material-ui dependencies

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,6 +4,40 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+// callback = function (error, httpStatus, responseText);
+function authenticatedXhr(method, url, callback) {
+  var retry = true;
+  function getTokenAndXhr() {
+    chrome.identity.getAuthToken({'interactive': true},
+      function (access_token) {
+        if (chrome.runtime.lastError) {
+          callback(chrome.runtime.lastError);
+          return;
+        }
+
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader('Authorization',
+          'Bearer ' + access_token);
+
+        xhr.onload = function () {
+          if (this.status === 401 && retry) {
+            // This status may indicate that the cached
+            // access token was invalid. Retry once with
+            // a fresh token.
+            retry = false;
+            chrome.identity.removeCachedAuthToken(
+              { 'token': access_token },
+              getTokenAndXhr);
+            return;
+          }
+
+          callback(null, this.status, this.responseText);
+        }
+      });
+  }
+}
+
+chrome.browserAction.setBadgeText({text: 'CL'});
 
 console.log('This is the Event Page for Browser Action');
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 'use strict';
 
 console.log('This is the Content script');
