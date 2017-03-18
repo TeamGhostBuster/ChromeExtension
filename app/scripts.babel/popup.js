c5c2659ffcce3b5e98509dd48ef34170fb727b67
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-18 15:53:11 -0600

    added ‘+’ icon to the popup, moved authentication to popup.js from background.js

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,37 @@
 'use strict';
 
 console.log('This is the Popup');
+
+// callback = function (error, httpStatus, responseText);
+function authenticatedXhr(method, url, callback) {
+  var retry = true;
+  function getTokenAndXhr() {
+    chrome.identity.getAuthToken({'interactive': true},
+      function (access_token) {
+        if (chrome.runtime.lastError) {
+          callback(chrome.runtime.lastError);
+          return;
+        }
+
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader('Authorization',
+          'Bearer ' + access_token);
+
+        xhr.onload = function () {
+          if (this.status === 401 && retry) {
+            // This status may indicate that the cached
+            // access token was invalid. Retry once with
+            // a fresh token.
+            retry = false;
+            chrome.identity.removeCachedAuthToken(
+              { 'token': access_token },
+              getTokenAndXhr);
+            return;
+          }
+
+          callback(null, this.status, this.responseText);
+        }
+      });
+  }
+}
