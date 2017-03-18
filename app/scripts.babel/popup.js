9f4e2fef50ab5f9235ccdfe5da0c67d9f275ccaa
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-18 17:37:49 -0600

    added content_security_policy to manifest, dropdown now works in browser

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -4,10 +4,12 @@ console.log('This is the Popup');
 
 
 
-callback = function(error, httpStatus, responseText) {
+function callback(error, httpStatus, responseText) {
   console.log('Response after authenticatedXhr to get lists:', httpStatus, responseText);
 }
 
 $('#listDropdown').on('show.bs.dropdown', function () {
-  authenticatedXhr('GET', 'https://demo.vfree.org/user.lists', callback)
+  authenticatedXhr('GET', 'https://api.vfree.org/user/lists', callback)
 })
+
+$('currentPagebtn').on()
