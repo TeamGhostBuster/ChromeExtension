a2bc19621ae1f9ddd338d58ffe0434ee05642ecc
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-19 17:03:46 -0600

    can now create article from current page, need better alerts

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,8 +1,14 @@
 'use strict';
 
 console.log('This is the Popup');
+var response_data;  // global variable for response of AJAX GET
+var selected_list = null;
 
 
+/**
+ * On loading the popup window, the system performs an GET request to get the user's lists
+ * and then calls populate_dropdown()
+ */
 window.onload = function () {
     chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
       console.log('Token:', token);
@@ -14,8 +20,9 @@ window.onload = function () {
           xhr.setRequestHeader('Access-Token', token);
         },
         success: function (data) {
-          console.log('Response after getting lists:', data);
-          populate_dropdown(data);
+          response_data = data;
+          console.log('Response after getting lists:', response_data);
+          populate_dropdown(response_data);
         },
         error: function(xhr, textStatus, errorThrown) {
           console.log('Request failed:', xhr, '\nStatus:',textStatus, '\nError:', errorThrown);
@@ -24,12 +31,16 @@ window.onload = function () {
     });
 }
 
+/**
+ * This function populates the dropdown for the Choose List button
+ * @param data the JSON result of the GET request from the onload function
+ */
 function populate_dropdown(data) {
   var list = document.getElementById('dropdownList');
   var index;
 
   for (index in data.lists) {
-    if (data.lists[index].archived != 'True') {
+    if (data.lists[index].archived == false) {
       var name = data.lists[index].name;
       var li = document.createElement('li');
       var link = document.createElement('a');
@@ -42,14 +53,62 @@ function populate_dropdown(data) {
   }
 }
 
+/**
+* This function updates the Choose List button with the selected option
+*/
 $('.dropdown-menu').on('click', 'li a', function(){
+  selected_list = $(this).text();
+  console.log('After selection, list:', selected_list);
   $(this).parents('.dropdown').find('.btn').html($(this).text() + ' <span class="caret"></span>');
   $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
 });
 
+/**
+ * Gets the URL of the currently open tab, generates the POST request
+ * TODO -- needs cleanup, messy af right now
+ */
 $('#currentPageBtn').on('click', function() {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var currentUrl = tabs[0].url;
-    console.log('Current URL:', currentUrl);
+    var page_title = tabs[0].title;
+    var description = 'Created by CL Extension.';
+    var data = {'title':page_title, 'description':description, 'url':currentUrl};
+    console.log('Data being sent:', JSON.stringify(data));
+
+    if (selected_list == null) {
+      alert('You must select a list!');  // TODO - use bootstrap alert
+    } else {
+      var list_id = get_list_id();
+      console.log('This is the id:', list_id);
+    }
+
+    chrome.identity.getAuthToken({'interactive': false}, function(token) {
+      $.ajax({
+        type:'POST',
+        url:'http://localhost:5000/user/list/'+list_id+'/article',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data),
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader('Access-Token', token);
+        },
+        success: function(textStatus) {
+          console.log('Request success:', textStatus);
+          // TODO -- implement bootstrap alerts
+        },
+        error: function(xhr, textStatus, errorThrown) {
+          console.log('Request failed:', xhr, '\nStatus:',textStatus, '\nError:', errorThrown);
+        }
+      });
+    });
   });
-})
+});
+
+function get_list_id() {
+  var index
+  for (index in response_data.lists) {
+    if (response_data.lists[index].name === selected_list) {
+      return response_data.lists[index].id;
+    }
+  }
+}
