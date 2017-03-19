f30d824505512657a2711b65308b1635f0ae5f95
Stephen Larsen <sdlarsen@ualberta.ca>
2017-03-19 15:07:45 -0600

    Started http requests, moved token access from background to popup

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,44 +1,9 @@
 'use strict';
 
+console.log('This is the Event Page for Browser Action');
+
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
 chrome.browserAction.setBadgeText({text: 'CL'});
-
-
-// callback = function (error, httpStatus, responseText);
-function authenticatedXhr(method, url, callback) {
-  var retry = true;
-  function getTokenAndXhr() {
-    chrome.identity.getAuthToken({'interactive': true},
-      function (access_token) {
-        if (chrome.runtime.lastError) {
-          callback(chrome.runtime.lastError);
-          return;
-        }
-
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.setRequestHeader('Authorization',
-          'Bearer ' + access_token);
-
-        xhr.onload = function () {
-          if (this.status === 401 && retry) {
-            // This status may indicate that the cached
-            // access token was invalid. Retry once with
-            // a fresh token.
-            retry = false;
-            chrome.identity.removeCachedAuthToken(
-              { 'token': access_token },
-              getTokenAndXhr);
-            return;
-          }
-
-          callback(null, this.status, this.responseText);
-        }
-      });
-  }
-}
-
-console.log('This is the Event Page for Browser Action');
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -2,15 +2,49 @@
 
 console.log('This is the Popup');
 
+// $('#listDropdown').on('show.bs.dropdown', function () {
+//
+// });
 
-
-function callback(error, httpStatus, responseText) {
-  console.log('Response after authenticatedXhr to get lists:', httpStatus, responseText);
+window.onload = function () {
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+      console.log('Token:', token);
+      $.ajax({
+        type: 'GET',
+        url: 'https://api.vfree.org/user/lists',
+        dataType: 'json',
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('Access-Token', token);
+        },
+        success: function (data) {
+          console.log('Response after getting lists:', data.view_data);
+          populate_dropdown(data.view_data);
+        },
+        error: function(xhr, textStatus, errorThrown) {
+          console.log('Request failed:', xhr, '\nStatus:',textStatus, '\nError:', errorThrown);
+        }
+      });
+    });
 }
 
-$('#listDropdown').on('show.bs.dropdown', function () {
-  authenticatedXhr('GET', 'https://api.vfree.org/user/lists', callback);
-})
+function populate_dropdown(data) {
+  var list = document.getElementById('dropdownList');
+  var index;
+
+  for (index in data.lists) {
+    if (data.lists[index].archived != 'True') {
+      var name = data.lists[index].name;
+      var li = document.createElement('li');
+      var link = document.createElement('a');
+      var text = document.createTextNode(name);
+      link.appendChild(text);
+      link.href = '#';
+      li.appendChild(link);
+      list.appendChild(li);
+    }
+  }
+}
+
 
 $('#currentPageBtn').on('click', function() {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
